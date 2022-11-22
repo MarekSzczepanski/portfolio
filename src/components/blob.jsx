ea6218862e949d2be7dba5e7067cbfc79a2882f7
Marek <marek@ilieth.at>
2022-11-22 14:38:11 +0100

    add skills animation

--- src/components/blob.jsx
+++ src/components/blob.jsx
@@ -11,28 +11,45 @@ const Morph = keyframes`
   80% { border-radius: 100% 60% 60% 100% / 100% 100% 60% 60%; }
   100% { border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%; }
 `
-const Move = (data = '0') => keyframes`
-  0% { transform: translate(${data.x1}, ${data.y1}), rotate(0deg) }
-  25% { transform: translate(${data.x2}, ${data.y2}) rotate(90deg) }
-  50% { transform: translate(${data.x3}, ${data.y3}) rotate(180deg) }
-  75% { transform: translate(${data.x4}, ${data.y4}) rotate(270deg) }
-  100% { transform: translate(${data.x5}, ${data.y5}), rotate(360deg) }
+const Move = (translate) => keyframes`
+  0% { transform: translate(${translate.x1}, ${translate.y1}) rotate(0deg) scale(1); }
+  12.5% { transform: translate(${translate.x2}, ${translate.y2}) rotate(45deg) scale(0.7); }
+  25% { transform: translate(${translate.x3}, ${translate.y3}) rotate(90deg) scale(0.6); }
+  37.5% { transform: translate(${translate.x4}, ${translate.y4}) rotate(135deg) scale(0.7); }
+  50% { transform: translate(${translate.x5}, ${translate.y5}) rotate(180deg) scale(1); }
+  62.5% { transform: translate(${translate.x6}, ${translate.y6}) rotate(225deg) scale(0.7); }
+  75% { transform: translate(${translate.x7}, ${translate.y7}) rotate(270deg) scale(0.6); }
+  87.5% { transform: translate(${translate.x8}, ${translate.y8}) rotate(315deg) scale(0.7); }
+  100% { transform: translate(${translate.x9}, ${translate.y9}) rotate(360deg) scale(1); }
+`
+const RotateBack = () => keyframes`
+  0% { transform: rotate(360deg); }
+  25% { transform: rotate(270deg); }
+  50% { transform: rotate(180deg); }
+  75% { transform: rotate(90deg); }
+  100% { transform: rotate(0deg); }
 `
 const Wrap = styled.div`
   display: flex;
   align-items: center;
-  justify-content: ${props => props.justify ? props.justify : 'center'};
 `
 const Circle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: ${props => props.position ? props.position : 'absolute'};
   top: ${props => props.top ? props.top : '0'};
   left: ${props => props.left ? props.left : '0'};
   width: ${props => props.measure};
   height: ${props => props.measure};
+  margin: ${props => props.margin};
   border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
-  background-color: ${props => props.color};
+  background: ${props => props.color ? props.color : '#F8F8ff'};
+  border: solid 3px ${props => props.border ? props.border : '#000'};
   overflow: hidden;
-  animation: ${Morph} 10s linear infinite ${props => props.delay ? props.delay : null}, ${props => props.noMove ? null : Move(props.translate)} 13s linear infinite;
+  transform: translate(${props => props.skill ? '-1500px, 150px' : 0}) scale(${props => props.skill ? '0.6' : '1'});
+  animation: ${Morph} 10s linear infinite ${props => props.delay ? props.delay : 0}s, ${props => props.noMove ? null : Move(props.translate)} 23s linear infinite ${props => props.delay ? props.delay : 0}s;
+  z-index: ${props => props.zIndex ? props.zIndex : 0};
   @media (max-width: 1023px) {
     position: absolute;
     top: ${props => props.mTop};
@@ -40,7 +57,15 @@ const Circle = styled.div`
     width: ${props => props.mMeasure};
     height: ${props => props.mMeasure};
   }
+  > div {
+    width: 100%;
+  }
 `
+const Text = styled.span`
+  animation: ${RotateBack} 23s linear infinite ${props => props.delay ? props.delay : 0}s;
+  font-size: ${props => props.fontSize ? props.fontSize : '1vw'};
+`
+
 const ProfileImage = () => {
     return (
       <Image
@@ -53,11 +78,12 @@ const ProfileImage = () => {
     );
   };
 
-const Blob = ({ color, measure, top, left, delay, mMeasure, mTop, mLeft, translate, noMove, image, position }) => {
+const Blob = ({ color, measure, top, left, delay, mMeasure, mTop, mLeft, translate, noMove, image, position, border, text, skill, zIndex, fontSize }) => {
     return ( 
         <Wrap>
-            <Circle measure={measure} color={color} top={top} left={left} delay={delay} mMeasure={mMeasure} mTop={mTop} mLeft={mLeft} translate={translate} noMove={noMove} position={position}>
+            <Circle measure={measure} color={color} top={top} left={left} delay={delay} mMeasure={mMeasure} mTop={mTop} mLeft={mLeft} translate={translate} noMove={noMove} position={position} border={border} skill={skill} zIndex={zIndex}>
                 {image ? <ProfileImage></ProfileImage> : null}
+                {text ? <Text delay={delay} fontSize={fontSize}>{text}</Text> : null}
             </Circle>
         </Wrap>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,32 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Hero from '../components/hero'
+import styled from "styled-components"
+import Blob from '../components/blob'
+
+const translate = {x1: '0vw', x2: '7vw', x3: '20vw', x4: '30vw', x5: '40vw', x6: '30vw', x7: '20vw', x8: '7vw', x9: '0vw', y1: '200px', y2: '400px', y3: '450px', y4: '400px', y5: '200px', y6: '50px', y7: '0px', y8: '50px', y9: '200px'};
+const skills = ['html', 'css', 'git', 'mysql', 'mongodb', 'express', 'cypress', 'jest', 'gatsby', 'gsap', 'percy', 'graphql', 'rest', 'ci / cd', 'npm', 'eslint', 'webpack'];
+const colors = ['#FF003F', 'gold', '#6787E7', '#74B72E', 'orange', 'plum', '#FF003F', 'gold', '#6787E7', '#74B72E', 'orange', 'plum', '#FF003F', 'gold', '#6787E7', '#74B72E', 'orange',];
+
+const SkillsContainer = styled.div`
+  position: relative;
+  flex-wrap: wrap;
+  height: 600px;
+  font-size: 1vw;
+`
 
 const IndexPage = () => (
   <Layout>
     <Hero></Hero>
     <h3>My skills</h3>
+    <SkillsContainer>
+      {skills.map(function(object, i){
+          return <Blob key={i} color={colors[i]} border={'black'} text={skills[i]} delay={i * 1.36} zIndex={i} skill={true} translate={translate} measure="9vw" mMeasure="49vw" />;
+      })}
+      <Blob color={'#f0DB4F'} border={'black'} text={'javascript'} skill={true} zIndex={21} fontSize={'1.5vw'} translate={{x1: '11vw', x2: '17vw', x3: '22vw', x4: '20vw', x5: '20vw', x6: '22vw', x7: '20vw', x8: '17vw', x9: '11vw', y1: '80px', y2: '130px', y3: '150px', y4: '130px', y5: '120px', y6: '110px', y7: '105px', y8: '100px', y9: '80px'}} measure="16vw" mMeasure="49vw" />
+      <Blob color={'#61DBFB'} border={'black'} text={'react'} skill={true} zIndex={22} delay={.5} fontSize={'1.5vw'} translate={{x1: '11vw', x2: '9vw', x3: '9vw', x4: '11vw', x5: '11vw', x6: '10vw', x7: '11vw', x8: '10vw', x9: '11vw', y1: '200px', y2: '230px', y3: '250px', y4: '230px', y5: '220px', y6: '210px', y7: '205px', y8: '200px', y9: '200px'}} measure="15vw" mMeasure="49vw" />
+      <Blob color={'#68A063'} border={'black'} text={'nodejs'} skill={true} zIndex={20} delay={1} fontSize={'1.5vw'} translate={{x1: '23vw', x2: '19vw', x3: '23vw', x4: '20vw', x5: '21vw', x6: '19vw', x7: '25vw', x8: '27vw', x9: '23vw', y1: '220px', y2: '270px', y3: '280px', y4: '270px', y5: '280px', y6: '250px', y7: '220px', y8: '210px', y9: '220px'}} measure="14vw" mMeasure="49vw" />
+    </SkillsContainer>
   </Layout>
 )
 
