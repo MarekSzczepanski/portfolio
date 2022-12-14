71e5446926b557ac5b4d5239eb0cb1e59a737498
Marek <marek@ilieth.at>
2022-12-14 01:26:36 +0100

    Change contact blob animation points

--- src/components/contactButton.jsx
+++ src/components/contactButton.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Blob from '../components/blob';
 
-const translate = {x1: '77vw', x2: '73vw', x3: '76vw', x4: '70vw', x5: '72vw', x6: '75vw', x7: '70vw', x8: '73vw', x9: '77vw', y1: '32vw', y2: '35vw', y3: '32vw', y4: '34vw', y5: '32vw', y6: '33vw', y7: '33vw', y8: '32vw', y9: '32vw'};
+const translate = {x1: '75vw', x2: '77vw', x3: '76vw', x4: '80vw', x5: '82vw', x6: '78vw', x7: '81vw', x8: '73vw', x9: '75vw', y1: '29vw', y2: '31vw', y3: '32vw', y4: '34vw', y5: '32vw', y6: '33vw', y7: '33vw', y8: '32vw', y9: '29vw'};
 const mTranslate = {x1: '2vw', x2: '7vw', x3: '16vw', x4: '14vw', x5: '8vw', x6: '5vw', x7: '15vw', x8: '9vw', x9: '2vw', y1: '6px', y2: '16px', y3: '14px', y4: '10px', y5: '11px', y6: '18px', y7: '8px', y8: '9px', y9: '6px'};
 
 const ContactButton = () => (
