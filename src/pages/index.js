f4ab7ee8ff7b081253ede882d949f9b06e928097
Marek <marek@ilieth.at>
2023-05-01 21:45:46 +0200

    Add magia project

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,6 +33,44 @@ const Wrap = styled.div`
   }
 `
 
+const projects = [
+  {
+    color: '#74B72E',
+    innerBlobColor: '#1a1e23',
+    image: 'magia.png',
+    text: 'Magiainternetu .com',
+    delay: '-9s',
+    href: 'https://magiainternetu.com/strony-internetowe-ostroda/',
+  },
+  {
+    color: 'royalblue',
+    image: 'btc.png',
+    customClass: 'project1',
+    text: 'Crypto Portfolio',
+    delay: '-1s',
+    href: 'https://cryptocalc.vyost.usermd.net/',
+  },
+  {
+    color: 'gold',
+    fontColor: '#333',
+    innerBlobColor: '#eee',
+    image: 'uploader.png',
+    customClass: 'project2',
+    text: 'Video Uploader',
+    delay: '-15s',
+    href: 'https://github.com/MarekSzczepanski/Video-Uploader',
+  },
+  {
+    color: 'crimson',
+    innerBlobColor: '#ddd',
+    image: 'swords.png',
+    customClass: 'project3',
+    text: 'Simple Adventure',
+    delay: '-6s',
+    href: 'https://marekszczepanski.github.io/Simple-Adventure/',
+  },
+]
+
 const IndexPage = () => (
   <Layout>
     <Hero></Hero>
@@ -44,9 +82,11 @@ const IndexPage = () => (
     <Wrap>
       <h3>projects</h3>
       <ProjectsContainer>
-        <Project color='royalblue' image='btc.png' customClass='project1' text='Crypto Portfolio' delay='-1s' href='https://cryptocalc.vyost.usermd.net/'></Project>
-        <Project color='gold' fontColor='#333' innerBlobColor='#eee' image='uploader.png' customClass='project2' text='Video Uploader' delay='-15s' href='https://github.com/MarekSzczepanski/Video-Uploader'></Project>
-        <Project color='crimson' innerBlobColor='#ddd' image='swords.png' customClass='project3' text='Simple Adventure' delay='-6s' href='https://marekszczepanski.github.io/Simple-Adventure/'></Project>
+        {projects.map((project, i) => {
+          return <Project customClass={project.customClass} color={project.color} fontColor={project.fontColor}
+           innerBlobColor={project.innerBlobColor} image={project.image} text={project.text}
+           delay={project.delay} href={project.href} key={i}></Project>
+        })}
       </ProjectsContainer>
     </Wrap>
     <ThingsILike></ThingsILike>
